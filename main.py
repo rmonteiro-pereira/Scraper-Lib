7398c09f638bb8ef951647017f7c17ec1a2e8f54
Rodrigo Monteiro Pereira <rmonteiropereira1@gmail.com>
2025-04-17 16:35:05 -0300

    feat: Enhance logging functionality and add new arguments for progress control in scraper

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ if __name__ == "__main__":
     base_url="https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page",
     file_patterns=[".csv", ".parquet", ".zip"],
     download_dir="tlc_data",
-    incremental=False,
+    incremental=True,
     max_files=10,
     max_concurrent=16,
+    dataset_name="TLC DATA",
     )
