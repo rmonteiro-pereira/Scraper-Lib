f78a4b7fa978d35f07309fbb1cdfb4629c70c776
rmonteiro-pereira <rmonteiropereira1@gmail.com>
2025-04-19 04:30:08 -0300

    Enhance DownloadState class with detailed docstrings and comments
    - Added comprehensive docstrings to all methods and the class itself to improve code readability and maintainability.
    - Updated comments to clarify the purpose of various code sections, particularly in the delay handling and state management methods.
    - Ensured consistent formatting and style throughout the code for better clarity.

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
-from package import scraper
+from package import ScraperLib
 import ray
 import logging
 import os
+
 if __name__ == "__main__":
     ray.init(
-    num_cpus=16,
-    include_dashboard=False,
-    logging_level=logging.ERROR,
-    ignore_reinit_error=True,
+        num_cpus=16,
+        include_dashboard=False,
+        logging_level=logging.ERROR,
+        ignore_reinit_error=True,
     )
     for i in range(14):
         print(f"Beginning {i}")
-        scraper(
-        base_url="https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page",
-        file_patterns=[".csv", ".parquet", ".zip"],
-        download_dir="tlc_data",
-        incremental=True,
-        max_files=2,
-        max_concurrent=16,
-        dataset_name="TLC DATA",
-        max_old_logs=10,
-        max_old_runs=10,
-        disable_terminal_logging=True,  
-        ray_instance=ray
+        scraper = ScraperLib(
+            base_url="https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page",
+            file_patterns=[".csv", ".parquet", ".zip"],
+            download_dir="tlc_data",
+            incremental=True,
+            max_files=2,
+            max_concurrent=16,
+            dataset_name="TLC DATA",
+            max_old_logs=10,
+            max_old_runs=10,
+            disable_terminal_logging=True,
+            ray_instance=ray
         )
+        scraper.run()
     # Count how many .log files are in the logs directory
     log_dir = "./logs"
     log_count = len([f for f in os.listdir(log_dir) if f.endswith(".log")])
